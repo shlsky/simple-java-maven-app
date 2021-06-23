cfb94aaf9ad2739f369d4673ce57c597d15b06e7
charles.song <charles.song@yijinin.com>
2021-06-23 20:26:06 +0800

    完善代码

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -11,7 +11,7 @@ set +x
 echo 'The following complex command extracts the value of the <name/> element'
 echo 'within <project/> of your Java/Maven project''s "pom.xml" file.'
 set -x
-NAME=`mvn help:evaluate -Dexpression=project.name | grep "^[^\[]"`
+NAME='my-app'
 set +x
 
 echo 'The following complex command behaves similarly to the previous one but'
